a444f4ebd9a0e12d741e94a4644ec08b50f48a97
Your Name <Your Name>
2023-02-05 00:57:01 +0200

    Home

--- app/src/main/java/com/example/cookit/home/view/HomePageFragment.java
+++ app/src/main/java/com/example/cookit/home/view/HomePageFragment.java
@@ -2,65 +2,100 @@ package com.example.cookit.home.view;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.example.cookit.R;
+import com.example.cookit.model.Category;
+import com.example.cookit.model.Meal;
+import java.util.Arrays;
+import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomePageFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HomePageFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HomePageFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomePageFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomePageFragment newInstance(String param1, String param2) {
-        HomePageFragment fragment = new HomePageFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    RecyclerView viewPager2;
+    RecyclerView category;
+    RecyclerView country;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_page, container, false);
+        View view = inflater.inflate(R.layout.fragment_home_page, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        viewPager2 = view.findViewById(R.id.viewPager);
+        viewPager2.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(view.getContext());
+        linearLayoutManager.setOrientation(RecyclerView.HORIZONTAL);
+        viewPager2.setLayoutManager(linearLayoutManager);
+        List<Meal> meals = Arrays.asList(new Meal(
+                        "Spicy Arrabiata Penne","https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg"),
+                new Meal("Spicy Arrabiata Penne",
+                        "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg"));
+        ViewPagerAdepter viewPagerAdepter = new ViewPagerAdepter(view.getContext(),meals);
+        viewPager2.setAdapter(viewPagerAdepter);
+
+
+
+        category = view.findViewById(R.id.categoryRecycler);
+        category.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager2 = new LinearLayoutManager(view.getContext());
+        linearLayoutManager2.setOrientation(RecyclerView.HORIZONTAL);
+        category.setLayoutManager(linearLayoutManager2);
+        List<Category> categories = Arrays.asList(new Category(
+                        "Beef")
+                ,new Category("Beef"
+                ),new Category("Beef"
+                ),new Category("Beef"
+                ),new Category("Beef"
+                ),new Category("Beef"
+                ),new Category("Beef"
+                ),new Category("Beef"
+                ));
+        RecycleCategoryAdepter recycleCategoryAdepter = new RecycleCategoryAdepter(view.getContext(),categories);
+        category.setAdapter(recycleCategoryAdepter);
+
+
+
+        country = view.findViewById(R.id.countryRecycler);
+        country.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager3 = new LinearLayoutManager(view.getContext());
+        linearLayoutManager3.setOrientation(RecyclerView.HORIZONTAL);
+        country.setLayoutManager(linearLayoutManager3);
+        List<Category> countries = Arrays.asList(new Category(
+                        "Egypt")
+                ,new Category("Egypt"
+                ),new Category("Egypt"
+                ),new Category("Egypt"
+                ),new Category("Egypt"
+                ),new Category("Egypt"
+                ),new Category("Egypt"
+                ),new Category("Egypt"
+                ));
+        RecycleCountryAdepter recycleCountryAdepter = new RecycleCountryAdepter(view.getContext(),countries);
+        country.setAdapter(recycleCountryAdepter);
+
+
+
+
+
     }
 }
