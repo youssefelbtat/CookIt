43d3e0aa00fd8a7e14ae3860f4bfd70c6dcab48b
Youssef Elbtat <yelbtat@gmail.com>
2023-02-12 23:06:37 +0200

    handel connectivity

--- app/src/main/java/com/example/cookit/utalites/Utalites.java
+++ app/src/main/java/com/example/cookit/utalites/Utalites.java
@@ -1,18 +1,29 @@
 package com.example.cookit.utalites;
 
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
 import com.example.cookit.model.MealModel;
-import com.example.cookit.model.modelFirebase.UserModel;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Utalites {
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager != null ? connectivityManager.getActiveNetworkInfo() : null;
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
 
     public static List<MealModel> FavUserMealsList=new ArrayList<>();
     public static final String SHARDPREFERENCE = "SharedPreference";
     public static final String USERNAME = "USERNAME";
     public static final String EMAIL = "EMAIL";
     public static final String PASSWORD = "PASSWORD";
+
     public static String SKIP ;
     public static final String IMAGE = "IMAGE";
     public static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
--- app/src/main/java/com/example/cookit/view/MainActivity.java
+++ app/src/main/java/com/example/cookit/view/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.cookit.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import android.content.pm.ActivityInfo;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkRequest;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.cookit.R;
+import com.example.cookit.utalites.Utalites;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
@@ -18,15 +24,47 @@ public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavView;
     FloatingActionButton floatingActionButton;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         floatingActionButton=findViewById(R.id.floatingButton);
         getSupportActionBar().hide();
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        init();
          bottomNavView=findViewById(R.id.bottomNavView);
          bottomNavView.setBackground(null);
-//       bottomNavView.getMenu().getItem(2).setEnabled(false);
+        System.out.println(Utalites.SKIP);
+         if(Utalites.SKIP=="skip"){
+             floatingActionButton.setEnabled(false);
+             bottomNavView.getMenu().getItem(2).setEnabled(false);
+             bottomNavView.getMenu().getItem(3).setEnabled(false);
+             bottomNavView.getMenu().getItem(4).setEnabled(false);
+         }
+         else if(!Utalites.isNetworkAvailable(this)){
+             bottomNavView.getMenu().getItem(4).setEnabled(false);
+         }
+        new ConnectivityManager.NetworkCallback() {
+            @Override
+            public void onAvailable(@NonNull Network network) {
+                super.onAvailable(network);
+                bottomNavView.getMenu().getItem(4).setEnabled(true);
+                System.out.println("There is internet");
+
+            }
+
+            @Override
+            public void onLost(@NonNull Network network) {
+                super.onLost(network);
+                System.out.println("There is no internet");
+                bottomNavView.getMenu().getItem(4).setEnabled(false);
+            }
+
+            @Override
+            public void onCapabilitiesChanged(@NonNull Network network, @NonNull NetworkCapabilities networkCapabilities) {
+                super.onCapabilitiesChanged(network, networkCapabilities);
+            }
+        };
 
         NavController navController= Navigation.findNavController(this,R.id.nav_host_fragment);
         NavigationUI.setupWithNavController(bottomNavView,navController);
@@ -37,5 +75,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+    }
+    void init(){
+        new NetworkRequest.Builder()
+                .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+                .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+                .build();
     }
 }
